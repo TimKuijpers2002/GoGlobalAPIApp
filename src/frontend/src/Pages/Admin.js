e3623eb8d2ca467554d2e35e1e6ccfd41694be42
TimKuijpers2002 <timkuijpers2002@outlook.com>
2021-10-06 10:14:18 +0200

    frontend now has reusable sidebar component

--- src/frontend/src/Pages/Admin.js
+++ src/frontend/src/Pages/Admin.js
@@ -1,9 +1,8 @@
 import React, {useEffect, useState} from "react";
-import {Breadcrumb, Empty, Layout, Menu, Spin, Table} from "antd";
-import {EnvironmentOutlined, InboxOutlined, LoadingOutlined, TeamOutlined, UserOutlined} from "@ant-design/icons";
-import {getAllAdmins} from "../Models/admin";
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+import {Empty, Spin, Table} from "antd";
+import {LoadingOutlined} from "@ant-design/icons";
+import {getAllAdmins} from "../Services/admin";
+import {Sidebar} from "../Components/Sidebar"
 
 const columns = [
     {
@@ -31,7 +30,6 @@ const columns = [
 const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 export default function Admin() {
     const [admins, setAdmins] = useState([]);
-    const [collapsed, setCollapsed] = useState(false);
     const [fetching, setFetching] = useState(true);
 
     const fetchAdmins = () =>
@@ -67,40 +65,10 @@ export default function Admin() {
 
     }
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-            <Sider collapsible collapsed={collapsed}
-                   onCollapse={setCollapsed}>
-                <div className="./logo" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                    <Menu.Item key="1" icon={<TeamOutlined />} onClick={() => {window.location.pathname = "/Admin"}}>
-                        Admins
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<InboxOutlined />} onClick={() => {window.location.pathname = "/LocationSubmit"}}>
-                        Location submits
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<InboxOutlined />} onClick={() => {window.location.pathname = "/Report"}}>
-                        Reports
-                    </Menu.Item>
-                    <Menu.Item key="4" icon={<UserOutlined />} onClick={() => {window.location.pathname = "/Profile"}}>
-                        Profile
-                    </Menu.Item>
-                    <Menu.Item key="5" icon={<EnvironmentOutlined />} onClick={() => {window.location.pathname = "/Location"}}>
-                        Locations
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }} />
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Admins</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                        {renderAdmins()}
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>By Tim Kuijpers ©2021</Footer>
-            </Layout>
-        </Layout>
+        <Sidebar data={
+            <div>
+                {renderAdmins()}
+            </div>
+        }/>
     );
 }
--- src/frontend/src/Pages/Home.js
+++ src/frontend/src/Pages/Home.js
@@ -1,46 +1,12 @@
-import React, {useState} from "react";
-import {Breadcrumb, Layout, Empty, Menu} from "antd";
-import {EnvironmentOutlined, InboxOutlined, TeamOutlined, UserOutlined} from "@ant-design/icons";
-import Sider from "antd/es/layout/Sider";
-const {Content, Footer, Header} = Layout;
+import React from "react";
+import {Sidebar} from "../Components/Sidebar";
 
 export default function Home() {
-    const [collapsed, setCollapsed] = useState(false);
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-            <Sider collapsible collapsed={collapsed}
-                   onCollapse={setCollapsed}>
-                <div className="logo" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                    <Menu.Item key="1" icon={<TeamOutlined />} onClick={() => {window.location.pathname = "/Admin"}}>
-                        Admins
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<InboxOutlined />} onClick={() => {window.location.pathname = "/LocationSubmit"}}>
-                        Location submits
-                    </Menu.Item>
-                    <Menu.Item key="3" icon={<InboxOutlined />} onClick={() => {window.location.pathname = "/Report"}}>
-                        Reports
-                    </Menu.Item>
-                    <Menu.Item key="4" icon={<UserOutlined />} onClick={() => {window.location.pathname = "/Profile"}}>
-                        Profile
-                    </Menu.Item>
-                    <Menu.Item key="5" icon={<EnvironmentOutlined />} onClick={() => {window.location.pathname = "/Location"}}>
-                        Locations
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-background" style={{ padding: 0 }} />
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                        Hello, welcome to work!
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>By Tim Kuijpers ©2021</Footer>
-            </Layout>
-        </Layout>
+        <Sidebar data={
+            <div>
+
+            </div>
+        }/>
     );
 }

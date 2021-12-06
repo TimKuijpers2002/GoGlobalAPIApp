756a62891bcc1fba6e097e987b97a0073ddc3e60
TimKuijpers2002 <timkuijpers2002@outlook.com>
2021-12-06 12:00:08 +0100

    made tests for admincontroller

--- src/test/java/GoGlobalProject/APIApp/UnitTests/AdminControllerTest.java
+++ src/test/java/GoGlobalProject/APIApp/UnitTests/AdminControllerTest.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -42,11 +43,11 @@ public class AdminControllerTest {
         when(adminService.GetAll()).thenReturn(list); //
 
         //Act
-        ResponseEntity<?> result = controller.GetAllAdmin();
+        ResponseEntity<List<Admin>> result = controller.GetAllAdmin();
 
         //Assert
-        assertThat(result.getStatusCode().toString())
-                .isEqualTo("200 OK");
+        assertThat(result.getStatusCodeValue())
+                .isEqualTo(200);
     }
 
     @Test
@@ -57,10 +58,65 @@ public class AdminControllerTest {
         when(adminRepository.findById(0L)).thenReturn(java.util.Optional.of(admin)); //
 
         //Act
-        ResponseEntity<?> result = controller.GetAdminById(0L);
+        ResponseEntity<Admin> result = controller.GetAdminById(0L);
 
         //Assert
-        assertThat(result.getStatusCode().toString())
-                .isEqualTo("200 OK");
+        assertThat(result.getStatusCodeValue())
+                .isEqualTo(200);
+    }
+
+    @Test
+    public void ShouldNOTPOSTAdmin_WhenDoubleEmail_isGiven(){
+        //Arrange
+        List<Admin> list = new ArrayList<>();
+        Admin empOne = new Admin(1L, "John", "test1@email.nl", "password1");
+        Admin empTwo = new Admin(2L, "Alex", "test2@email.nl","password2");
+        Admin empThree = new Admin(3L, "Steve", "test3@email.nl","password3");
+
+        list.add(empOne);
+        list.add(empTwo);
+        list.add(empThree);
+
+        when(adminService.GetAll()).thenReturn(list); // Mock
+
+        //Act
+        Admin admin = new Admin(0L, "John","test1@email.nl","password");
+        when(adminService.Create(any(Admin.class))).thenReturn(true);
+        when(adminService.CheckForDoubleEmails(any(Admin.class))).thenReturn(true);
+
+        ResponseEntity<Admin> response = controller.CreateAdmin(admin);
+
+        //Assert
+        assertThat(response.getStatusCodeValue())
+                .isEqualTo(409);
+    }
+
+    @Test
+    public void ShouldDELETEAdmin_WhenIDisGiven() throws ResourceNotFoundException {
+        //Arrange
+        Admin adminToDelete = new Admin(1L, null, null,null);
+
+        //Act
+        when(adminRepository.findById(1L)).thenReturn(java.util.Optional.of(adminToDelete)); // Mock
+        ResponseEntity<Admin> response = controller.DeleteAdmin(adminToDelete.getAdminId());
+
+        //Assert
+        assertThat(response.getStatusCodeValue())
+                .isEqualTo(200);
+    }
+
+    @Test
+    public void ShouldPOSTAdmin_WhenDataGiven(){
+        //Arrange
+        Admin admin = new Admin(1L, "John", "test1@email.nl", "password1");
+        when(adminService.Create(any(Admin.class))).thenReturn(false);
+        when(adminService.CheckForDoubleEmails(any(Admin.class))).thenReturn(false);
+
+        //Act
+        ResponseEntity<Admin> response = controller.CreateAdmin(admin);
+
+        //Assert
+        assertThat(response.getBody())
+                .isEqualTo(admin);
     }
 }
